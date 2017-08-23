626b80352d891b2de984432c2ec4e0344f653c0e
Omkar <omkar@activesphere.com>
2017-08-23 16:33:22 +0530

    Initial Commit

---
+++ js/Main.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var Config = require("./Config");
+var Particles = require("./particles/Particles");
+var Webcam = require("lib/anonymous/components/webcam/Webcam");
+var AModule = require("modules/AModule");
+
+/**
+ * Main
+ * @constructor
+ */
+var Main = function() {
+  AModule.apply(this);
+
+  this.init();
+};
+
+Main.prototype = $.extend({}, AModule.prototype, {
+  init: function() {
+    AModule.prototype.init.call(this);
+
+    //
+    this._content = $("#particles");
+
+    // particles
+    this._particles = new Particles(this._content[0]);
+
+    // webcam
+    this._webcam = new Webcam({
+      dom: document.querySelector(".source__video"),
+      video: true,
+      audio: false
+    });
+    this._webcam.$.on("success", $.proxy(this._onWebcamSuccess, this));
+  },
+
+  /**
+	 * Drawing on requestAnimationFrame
+	 */
+  update: function() {
+    this._particles.update();
+
+    AModule.prototype.update.call(this);
+  },
+
+  /**
+	 * Triggered on window resize
+	 */
+  _onResize: function() {
+    AModule.prototype._onResize.call(this);
+
+    this._particles.resize();
+  },
+
+  //-----------------------------------------------------o webcam handlers
+
+  _onWebcamSuccess: function() {
+    this._particles.setTexture(this._webcam.dom, true);
+  }
+});
+
+/**
+ * Let's roll
+ */
+Stage.$document.ready(function() {
+  var main = new Main();
+
+  (function tick() {
+    main.update();
+    window.requestAnimationFrame(tick);
+  })();
+});
---
+++ js/modules/AModule.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var $ = require("lib/zepto/zepto");
+var RequestAnimationFrame = require("lib/anonymous/utils/RequestAnimationFrame");
+var Stats = require("lib/stats/Stats");
+var Stage = require("lib/anonymous/core/Stage");
+var Easing = require("lib/zepto/Easing");
+
+/**
+ * AModule abstract class
+ * @constructor
+ */
+var AModule = (module.exports = function() {});
+
+AModule.prototype = {
+  init: function() {
+    this._displayCredits();
+
+    $(".link--sources").animate({ opacity: 1 }, 500, Easing.outQuad, null, 500);
+
+    // stats
+    this._stats = new Stats();
+    document.body.appendChild(this._stats.domElement);
+
+    // resize
+    Stage.$window.on("resize", $.proxy(this._onResize, this));
+    Stage.resize();
+  },
+
+  /**
+     * Drawing on requestAnimationFrame
+     */
+  update: function() {
+    this._stats.update();
+  },
+
+  /**
+     * Triggered on window resize
+     */
+  _onResize: function() {
+    Stage.resize();
+  },
+
+  _displayCredits: function() {
+    if ($.browser.webkit) {
+      console.log(
+        "%cʕʘᴥʘʔ︎",
+        "color: #A89E8F; font-size: 54px; font-family: courier new; line-height: 50px;"
+      );
+      console.log("");
+    }
+  }
+};
