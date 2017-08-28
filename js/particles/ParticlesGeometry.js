dc3d313de7ae3377e861e1f128f200329d322a22
Omkar <omkar@activesphere.com>
2017-08-28 14:25:37 +0530

    Update structure for ParticlesGeometry

--- js/particles/ParticlesGeometry.js
+++ js/particles/ParticlesGeometry.js
@@ -1,45 +1,35 @@
-"use strict";
+const THREE = require("./../lib/three");
 
-var THREE = require("./../lib/three");
-
-/**
- * ParticlesGeometry
- * @constructor
- */
-THREE.ParticlesGeometry = module.exports = function(width) {
-  this.width = width;
-  this.n = width * width;
-
-  THREE.BufferGeometry.call(this);
-
-  this.init();
-};
-
-THREE.ParticlesGeometry.prototype = Object.create(
-  THREE.BufferGeometry.prototype
-);
-
-THREE.ParticlesGeometry.prototype.init = function() {
-  this.positions = new Float32Array(this.n * 3);
-  this.uvs = new Float32Array(this.n * 2);
-  for (var i = 0, l = this.n * 3; i < l; i += 3) {
-    var index, entries;
-    entries = 3;
-
-    index = (i / entries) | 0;
-
-    var u = index % this.width / this.width;
-    var v = ((index / this.width) | 0) / this.width;
-    var z = 0;
-
-    var i3 = index * entries;
-    this.positions[i3] = u;
-    this.positions[i3 + 1] = v;
-    this.positions[i3 + 2] = z;
+class ParticlesGeometry extends THREE.BufferGeometry {
+  constructor(width) {
+    super(width);
+    this.width = width;
+    this.n = width * width;
+    this.init();
   }
 
-  this.addAttribute("uv", new THREE.BufferAttribute(this.uvs, 2));
-  this.addAttribute("position", new THREE.BufferAttribute(this.positions, 3));
-};
+  init() {
+    this.positions = new Float32Array(this.n * 3);
+    this.uvs = new Float32Array(this.n * 2);
+    for (var i = 0, l = this.n * 3; i < l; i += 3) {
+      var index, entries;
+      entries = 3;
+  
+      index = (i / entries) | 0;
+  
+      var u = index % this.width / this.width;
+      var v = ((index / this.width) | 0) / this.width;
+      var z = 0;
+  
+      var i3 = index * entries;
+      this.positions[i3] = u;
+      this.positions[i3 + 1] = v;
+      this.positions[i3 + 2] = z;
+    }
+  
+    this.addAttribute("uv", new THREE.BufferAttribute(this.uvs, 2));
+    this.addAttribute("position", new THREE.BufferAttribute(this.positions, 3));
+  }
+}
 
-THREE.ParticlesGeometry.prototype.resize = function(width, height) {};
+THREE.ParticlesGeometry = ParticlesGeometry;
