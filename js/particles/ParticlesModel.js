70ccb947124c5e1fb415c3ddd2cd769c3c0313a2
Omkar <omkar@activesphere.com>
2017-08-29 16:35:02 +0530

    Fine tuning to Model Params

--- js/particles/ParticlesModel.js
+++ js/particles/ParticlesModel.js
@@ -9,19 +9,19 @@ class ParticlesModel {
       // look & feel
       initialAttraction: 0,
       frictions: 0,
-      resetStacked: true,
+      resetStacked: false,
       stackSensibility: 0.8,
 
       // repulsion
       repulsion: true,
-      repulsionRadius: 0.5,
+      repulsionRadius: 0.25,
       repulsionStrength: 0.0050,
       repulsionSensibility: 0.8,
 
-      velocityMax: 0.0015,
-      mapStrength: 0.1500,
+      velocityMax: 0.0075,
+      mapStrength: 0.0195,
       pointSize: 1,
-      alpha: 0.25,
+      alpha: 0.23,
       inverted: true,
       particlesColor: "#ffffff",
       bgColor: "#4c4c4c"
